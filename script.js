4beb2b7f2d7ba53e40a8be44b4d1786a4d057410
Michal Leskiewicz <leskiewicz02@gmail.com>
2022-05-17 21:36:53 +0200

    Game Logics done

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 const gameboard = (() => {
+    // Define variables
     let _gameboard = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
     
-    // cache DOM
+    // Cache DOM
     let board = document.getElementById('board');
 
     createBoard();
-    
-    function createBoard() {
-        _gameboard.forEach(createButtons);
-    };
 
     function createButtons(val) {
         const newBtn = document.createElement('button');
+        newBtn.classList.add('fieldButton');
         if (val !== 'X' && val !== 'O') {
             newBtn.classList.add('field');
         } else if (val === 'X') {
@@ -23,52 +21,115 @@ const gameboard = (() => {
         board.appendChild(newBtn)
     };
 
-
-    const makeBoardChanges = (e, moveCount) => {
-        let button = e.target;
+    // Count moves to make changes in _gameboard for every move
+    const makeBoardChanges = (square, moveCount) => {
+        let button = square;
         let index = _gameboard.indexOf(button.getAttribute('btnnum'));
         if (moveCount % 2 == 0) {
             _gameboard[index] = 'X'
         } else {
             _gameboard[index] = 'O'
         }
+        game.bindToBoardKeys()
         game.checkForWin(_gameboard, moveCount)
+    };
+
+    function createBoard() {
+        board.innerHTML = '';
+        _gameboard.forEach(createButtons);
     }
 
     const resetBoard = () => {
-        board.innerHTML = '';
-        createBoard()
+        _gameboard = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
     }
     
-    return {makeBoardChanges, resetBoard}
+    return {makeBoardChanges, createBoard, resetBoard}
 })();
 
 
-
 const game = (() => {
+    // Cache DOM
+    const playButton = document.querySelector('#play');
+    const resetButton = document.querySelector('#reset');
+    const exitButton = document.querySelector('#exit');
+    const boardDisplay = document.querySelector('#boardDisplay');
 
-    let player1 = 'X'
-    let player2 = 'O'
+    // Bind events
+    playButton.addEventListener('click', clickPlay);
+    resetButton.addEventListener('click', resetGame);
+    exitButton.addEventListener('click', exitGame);
+
+    // Define variables
+    let playerOne;
+    let playerTwo;
+    let option;
+    let buttons;
     let moveCount = 1;
+    const nameOneDisplay = document.querySelector('#nameOneDisplay');
+    const nameTwoDisplay = document.querySelector('#nameTwoDisplay');
 
+    // Needed to play the first round
     bindToBoardKeys();
 
+    // Takes options from control panel and stars game
+    function clickPlay() {
+        displayController.play();
+        const players = displayController.play();
+        playerOne = players.players.playerOne;
+        playerTwo = players.players.playerTwo;
+        option = displayController.checkGameOption();
+        saveOption = displayController.checkGameOption();
+        if (option === 'pve' && playerOne.input === 'O'){
+            computerTurn();
+        };
+    };
+
+    // Selects all available buttons and binds function
+    // that makes them clickable
     function bindToBoardKeys() {
-        gameboard.resetBoard()
-        const buttons = document.querySelectorAll('.field');
+        gameboard.createBoard()
+        buttons = document.querySelectorAll('.field');
         buttons.forEach(item => {
-            item.addEventListener('click', oneMove);
+            item.addEventListener('click', move);
+        });
+    };
+
+    // After the game ends it makes all buttons unclickable
+    // and makes background darker
+    function boardKeysAfterEnd() {
+        buttons.forEach(item => {
+            item.removeEventListener('click', move);
+        });
+        const allButtons = document.querySelectorAll('.fieldButton');
+        allButtons.forEach(item => {
+            item.style.backgroundColor = 'rgb(22, 114, 228)';
         });
     }
     
-    function oneMove(e) {
+    // Makes a square unclickable after use,
+    // it also fires function creating board changes
+    // and creates new board.
+    // Also checks if someone won the game. with function
+    // inside makeBoardChanges
+    function move(e) {
         let square = e.target;
-        gameboard.makeBoardChanges(e, moveCount);
-        bindToBoardKeys();
         moveCount++;
+        gameboard.makeBoardChanges(square, moveCount);
         square.disabled = true;
+        if (option === 'pve') {
+            computerTurn()
+        };
     };
 
+    // One random computer move, also checks if someone
+    // won with makeboardchanges
+    function computerTurn() {
+        moveCount++;
+        buttons = document.querySelectorAll('.field');
+        let btnArray = Array.from(buttons);
+        let square = btnArray[Math.floor(Math.random()*btnArray.length)];
+        gameboard.makeBoardChanges(square, moveCount);
+    };
 
     const checkForWin = (board, moveCount) => {
         if (
@@ -99,7 +160,13 @@ const game = (() => {
             board[4] === 'X' &
             board[6] === 'X' 
         )  {
-            alert('player one won')
+            if (playerOne.input === 'X') {
+                boardKeysAfterEnd();
+                nameOneDisplay.style.boxShadow = '0 0 20px rgb(202, 22, 22)'
+            } else {
+                nameTwoDisplay.style.boxShadow = '0 0 20px rgb(202, 22, 22)'
+                boardKeysAfterEnd();
+            }
         } else if (
             board[0] === 'O' &
             board[1] === 'O' &
@@ -128,46 +195,151 @@ const game = (() => {
             board[4] === 'O' &
             board[6] === 'O' 
         ) {
-            alert('player two won')
-        } else if (moveCount === 9) {
-            alert('shot')
+            if (playerOne.input === 'O') {
+                boardKeysAfterEnd();
+                nameOneDisplay.style.boxShadow = '0 0 20px rgb(202, 22, 22)'
+            } else {
+                boardKeysAfterEnd();
+                nameTwoDisplay.style.boxShadow = '0 0 20px rgb(202, 22, 22)'
+            }
+        } else if (moveCount === 10) {
+            boardKeysAfterEnd();
+            alert('It\'s a tie!');
         };
     };
-    
-    return {checkForWin}
-})();
 
-const displayController = (() => {
-
-    // Cache DOM
-    const pveButton = document.querySelector('#pve');
-    const pvpButton = document.querySelector('#pvp');
-    const playButton = document.querySelector('#play');
-    let playerTwoNameInput =  document.querySelector('#playerTwoName');
-    
-    // Bind Events
-    playButton.addEventListener('click', play);
-    pvpButton.addEventListener('click', checkRadioButtons);
-    pveButton.addEventListener('click', checkRadioButtons)
-
-    function checkRadioButtons() {
-        if (pvpButton.checked) {
-            pvp();
-        } else if (pveButton.checked) {
-            pve();
+    // Resets gameboard, movecount and sets buttons color
+    // back to normal
+    function resetGame() {
+        gameboard.resetBoard();
+        bindToBoardKeys();
+        moveCount = 1;
+        const allButtons = document.querySelectorAll('.fieldButton');
+        allButtons.forEach(item => {
+            item.style.backgroundColor = 'rgb(171, 207, 251)';
+        });
+        nameOneDisplay.style.boxShadow = '0 0 7px rgb(0, 0, 0)';
+        nameTwoDisplay.style.boxShadow = '0 0 7px rgb(0, 0, 0)';
+        if (option === 'pve' && playerOne.input === 'O') {
+            computerTurn()
         }
     }
 
-    function play() {
-        document.querySelector('#options').style.display = "none"
-    }
-
-    function pve() {
-        playerTwoNameInput.classList.add('hidden')
-    }
-
-    function pvp() {
-        playerTwoNameInput.classList.remove('hidden')
-    }
-
+    // Resets game and shows menu
+    function exitGame() {
+        resetGame();
+        document.querySelector('#options').style.visibility = "visible";
+    };
+    
+    return {checkForWin, computerTurn, bindToBoardKeys};
 })();
+
+const displayController = (() => {
+    // Cache DOM
+    const pveButton = document.querySelector('#pve');
+    const pvpButton = document.querySelector('#pvp');
+    const playerOneNameInput = document.querySelector('#playerOneName');
+    const playerTwoNameInput =  document.querySelector('#playerTwoName');
+    const chooseX = document.querySelector('#X');
+    const chooseO = document.querySelector('#O');
+    const nameOneDisplay = document.querySelector('#nameOneDisplay');
+    const nameTwoDisplay = document.querySelector('#nameTwoDisplay');
+
+    // Bind Events
+    pvpButton.addEventListener('click', showPlayerTwoNameInput);
+    pveButton.addEventListener('click', showPlayerTwoNameInput);
+
+    // players constructor
+    const Player = (name, input) => {
+        return {name, input}
+    };
+
+    function showPlayerTwoNameInput() {
+        const option = checkGameOption();
+        if (option === 'pvp') {
+            playerTwoNameInput.classList.remove('hidden');
+        } else {
+            playerTwoNameInput.classList.add('hidden');
+        };
+    };
+
+    function checkGameOption() {
+        let option;
+        if (pvpButton.checked) {
+            option = 'pvp'
+        } else if (pveButton.checked) {
+            option = 'pve'
+        };
+        return option
+    };
+
+    function checkSelectedInput() {
+        let input;
+        if (chooseX.checked) {
+            input = 'X';
+        } else {
+            input = 'O';
+        };
+        return input
+    };
+
+    const play = function play() {
+        const option = checkGameOption();
+        let players;
+        if (option === 'pvp') {
+            pvp();
+            players = pvp();
+        } else {
+            pve();
+            players = pve()
+        };
+        document.querySelector('#options').style.visibility = "hidden";
+
+        return {players}
+    };
+
+    // Checks input to assign it to player,
+    // creates player one and player two (computer)
+    function pve() {
+        const inputOne = checkSelectedInput(); 
+        let inputTwo;
+        if (inputOne === 'X') {
+            inputTwo = 'O';
+        } else {
+            inputTwo = 'X';
+        };
+        const playerOne = Player(playerOneNameInput.value, inputOne); 
+        const playerTwo = Player('Computer', inputTwo);      
+        displayPlayersNames(playerOne, playerTwo);
+
+        return {playerOne, playerTwo};
+    };
+
+    // Checks input to assign it to player,
+    // creates player one and player two  
+    function pvp() {
+        playerTwoNameInput.classList.remove('hidden');
+        const inputOne = checkSelectedInput(); 
+        let inputTwo;
+        if (inputOne === 'X') {
+            inputTwo = 'O';
+        } else {
+            inputTwo = 'X';
+        };
+        const playerOne = Player(playerOneNameInput.value, inputOne);
+        const playerTwo = Player(playerTwoNameInput.value, inputTwo);
+        displayPlayersNames(playerOne, playerTwo);
+        
+        return {playerOne, playerTwo}
+    }
+
+    function displayPlayersNames(playerOne, playerTwo) {
+        nameOneDisplay.textContent = playerOne.name;
+        nameTwoDisplay.classList.remove('hidden');
+        nameTwoDisplay.textContent = playerTwo.name; 
+    }
+
+    return {play, checkGameOption}
+})();
+
+// mixnmaxing
